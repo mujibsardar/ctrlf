38f4b3972bd4eb7b4b6d7e4982c3299981544eb3
Clara Kang <clara.kang91@gmail.com>
2017-05-13 16:57:33 -0700

    auth packages install and define constants

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const
-  express = require('express'),
-  app = express(),
-  mongoose = require('mongoose'),
-  logger = require('morgan'),
-  ejs = require('ejs'),
-  ejsLayouts = require('express-ejs-layouts'),
-  bodyParser = require('body-parser'),
-  dotenv = require('dotenv').load({silent: true}),
+  express = require('express'), //router and controller
+  app = express(), //express app
+  mongoose = require('mongoose'), //mongoDB
+  logger = require('morgan'), //http request logs
+  ejs = require('ejs'), //view template
+  ejsLayouts = require('express-ejs-layouts'), //template layout handler
+  bodyParser = require('body-parser'), //json message translation to usable data
+  dotenv = require('dotenv').load({silent: true}), //key protection
   // Import Bing API package
   Bing = require('node-bing-api')({ accKey:  process.env.BING_SEARCH_ACCESS_TOKEN}),
+  flash = require('connect-flash'), //send msgs to a view depending on login/logout
+  cookieParser = require('cookie-parser'), //translates session data and make it accessible in our app
+  session = require('express-session'), //creates session cookies for authenticated users
+  MongoDBStore = require('connect-mongodb-session')(session), //session storage on mongoDB
+  passport = require('passport'), //middleware for authentication
   PORT = 3000
 
 
@@ -17,16 +22,12 @@ mongoose.connect('mongodb://localhost/web-meta-data', (err) => {
   console.log(err || 'Connected to mongodb!')
 })
 
-
 //establish a path to public folder: no route needed because we just need one file
 app.use(express.static(__dirname + '/public'))
-
 
 //middleware
 app.use(logger('dev'))
 app.use(bodyParser.json())
-
-
 
 //server
 app.listen(PORT, (err) => {
