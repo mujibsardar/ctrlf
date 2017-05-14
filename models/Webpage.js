e3f1f15dbf0992ce20e9861143724b9963ee235f
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-05-14 11:51:21 -0700

    Added comments to webpage model

--- models/Webpage.js
+++ models/Webpage.js
@@ -1,11 +1,13 @@
 // Webpage model
 const
   mongoose = require("mongoose"),
+  // Establish structure of embedded comments
   commentSchema = mongoose.Schema({
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // reference to a user as a foreign key
     name: String
   }),
+  // Model of webpage data
   webpageSchema = mongoose.Schema({
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
     url: String,
     comments: [commentSchema]
   })
