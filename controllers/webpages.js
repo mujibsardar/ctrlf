bfc85784bec87b736a88e66f73c37d5e4becbb6c
Jeremiah Haidze <haidzejeremiah@yahoo.com>
2017-05-15 15:31:25 -0700

    test commit

---
+++ controllers/webpages.js
@@ -0,0 +1,15 @@
+//Webpage controllers
+const
+  Webpage = require('../models/Webpage.js') //require the Webpage model to be able to create functions on the database
+
+module.exports = {
+  index: (req, res) => {
+
+  },
+  show: (req, res) => {
+    //you can show a sepcific webpage (if it exists in the database) by doing Webpage.findById()
+    //in the webpage router (not here)you can do something down this line:
+  //
+  }
+  //customized functions here
+}
