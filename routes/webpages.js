df9266c500afc856edec4a129e3e4f053ca4182f
Clara Kang <clara.kang91@gmail.com>
2017-05-15 13:51:41 -0700

    webpages controller created, linked in webpage routers

--- routes/webpages.js
+++ routes/webpages.js
@@ -1,8 +1,9 @@
-//Webpage router
+//webpage routes
 const
   express = require('express'),
-  webpageRouter = express.Router(),
-  Webpage = require('../models/Webpage.js')
+  webpageRouter = express.Router(), //create a router for webpage
+  Webpage = require('../models/Webpage.js'), //now that we have the controller file separated, this doesn't need to be required.
+  webpagesController = require('../controllers/webpages.js') //require webpages controller file
 
 webpageRouter.route('/show/')
   .post((req, res) => {
