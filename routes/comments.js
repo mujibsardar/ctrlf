300db9165cfdbc15af1cc277227734aa87d77c39
Clara Kang <clara.kang91@gmail.com>
2017-05-18 15:29:34 -0700

    edit comment

--- routes/comments.js
+++ routes/comments.js
@@ -16,7 +16,15 @@ commentsRouter.delete('/:id', (req, res) => {
     })
   })
 
-
+commentsRouter.patch('/:id', (req, res) => {
+  Comment.findByIdAndUpdate(req.params.id, req.body, {new: true}, function(err, updatedComment) {
+    if(err) {
+      res.json({message: "Couldn't update the comment", success: false})
+    } else {
+      res.json({message: "Comment Updated!", success: true, comment: updatedComment})
+    }
+  })
+})
 
 
 module.exports = commentsRouter
